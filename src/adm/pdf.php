4cb2758a620e81d858f49e3d13bd560aee8a466a
Leo-Santos17 <leonardosm@protonmail.com>
2024-11-22 17:10:45 -0300

    Viewer PDF

---
+++ src/adm/pdf.php
@@ -0,0 +1,22 @@
+<!-- Esse Arquivo é rascunho para jogar no arquivo original -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Visualizador e Download de PDF</title>
+</head>
+<body>
+    <h1>Visualize e Baixe o PDF</h1>
+    
+    <!-- Adicione dinamicamente o caminho do pdf no href -->
+    <h2>Visualize o PDF</h2>
+    <a href="URL.pdf">Visualizar PDF</a>
+
+    <!-- Adicione dinamicamente o caminho do pdf no src -->
+    <h2>Visualizar o PDF</h2>
+    <iframe src="END.pdf" width="70%" height="600px"></iframe>
+    
+    <!-- Adicione dinamicamente o caminho do pdf no href -->
+    <h2>Baixe o PDF</h2>
+    <a href="URL.pdf" download>Baixar PDF</a>
+</body>
+</html>
