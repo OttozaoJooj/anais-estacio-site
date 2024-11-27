13afb8faff75f1e9742ca7b2a420e9fad795c0dd
Otto Mozale <otto.mozale007@gmail.com>
2024-11-27 00:55:10 -0300

    add: front-end login/signin

--- src/login/login.php
+++ src/login/login.php
@@ -1,24 +1,53 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aba de Login</title>
     <link rel="stylesheet" href="../../static/styles/login.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
 </head>
 <body>
-    
-    <form action="login_proc.php" method="GET">
-        <div class="card">
-            <div class="container">
-                <h2 style="text-align: center;">Login</h2>
-            </div>
-            <label for="cod_docente">Código Docente:</label>
-            <input type="text" id="cod_docente" name="cod_docente" required><br><br>
-            <label for="senha">Senha:</label><br>
-            <input type="password" id="senha" name="senha"><br><br>
-            <input type="submit" value="Entrar">
+    <header>
+        <div class="header-img">
+            <img src="../../assets/img/logoEstacio.png">
         </div>
-    </form>
-    <a href="../signin/signin.php">Registre-se</a>
+    </header>
+
+    <div class="container">
+        <div class="content">
+            <div class="title">
+                <h1>ENTRAR</h1>
+            </div>
+            <form class="form" action="login_proc.php" method="GET">
+                <div class="form-content">
+                    <label for="cod_docente">Código Docente:</label>
+                    <input type="text" id="cod_docente" name="cod_docente" required>
+                    <label for="senha">Senha:</label>
+                    <input type="password" id="senha" name="senha" required>
+                    <input type="submit" value="Entrar">
+                </div>
+                <div class="link-signin">
+                    <a href="../signin/signin.php">Cadastre-se</a>
+                </div>
+                <div class="logo-estacio">
+                    <img src="../../assets/img/logoEstacio2.png" width="96px" height="27px">
+                </div>
+            </form>
+        </div>
+    </div>
+    <footer>
+        <p>Desenvolvido pelos Alunos do Curso de <strong>Analise e Desenvolvimento de Sistemas</strong>  da Estácio Castanhal.</p>
+        <p class="links">
+            <a href="https://www.linkedin.com/in/samuel-nicolas-974806252/">Samuel Nicolas</a>
+            <a href="https://www.linkedin.com/in/leonardo-santos-1172ba2a9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Leonardo Maciel</a>
+            <a href="#">Otto Mozale</a>
+        </p>
+        
+    </footer>
 </body>
 </html>
+
--- src/signin/signin.php
+++ src/signin/signin.php
@@ -1,30 +1,59 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Estácio Anais</title>
-    <link rel="stylesheet" href="login.css">
+    <title>Aba de Login</title>
+    <link rel="stylesheet" href="../../static/styles/signin.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
 </head>
 <body>
+    <header>
+        <div class="header-img">
+            <img src="../../assets/img/logoEstacio.png">
+        </div>
+    </header>
 
-    <div class="card">
-        <form action="signin_proc.php" method="post">
-            <label for="nome">Nome:</label><br>
-            <input type="text" name="nome" id=""><br><br>
-            <label for="text">Código Docente:</label><br>
-            <input type="cod_docente" name="cod_docente"><br><br>
-            <label for="senha">Senha:</label><br>
-            <input type="password" name="senha" id=""><br><br>
-            <label for="curso">Curso:</label><br><br>
-            <select name="curso" id="curso">
-                <option value="1" selected>Análise e Desenvolvimento de Sistemas</option>
-            </select><br><br>
-            <input type="submit" value="Enviar">
-        </form>
+    <div class="container">
+        <div class="content">
+            <div class="title">
+                <h1>CADASTRE-SE</h1>
+            </div>
+            <form class="form" action="signin_proc.php" method="GET">
+                <div class="form-content">
+                    <label for="nome">Nome:</label>
+                    <input type="text" name="nome" id="nome" required>
+                    <label for="text">Código Docente:</label>
+                    <input type="text" name="cod_docente" required>
+                    <label for="senha">Senha:</label>
+                    <input type="password" name="senha" id="senha" required>
+                    <label for="curso">Curso:</label>
+                    <select name="curso" id="curso" required>
+                        <option value="1" selected>Análise e Desenvolvimento de Sistemas</option>
+                    </select>
+                    <input type="submit" value="Cadastrar">
+                </div>
+                <div class="link-login">
+                    <a href="../login/login.php">Entrar</a>
+                </div>
+                <div class="logo-estacio">
+                    <img src="../../assets/img/logoEstacio2.png" width="96px" height="27px">
+                </div>
+            </form>
+        </div>
     </div>
-
-     
-   
+    <footer>
+        <p>Desenvolvido pelos Alunos do Curso de <strong>Analise e Desenvolvimento de Sistemas</strong>  da Estácio Castanhal.</p>
+        <p class="links">
+            <a href="https://www.linkedin.com/in/samuel-nicolas-974806252/">Samuel Nicolas</a>
+            <a href="https://www.linkedin.com/in/leonardo-santos-1172ba2a9?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Leonardo Maciel</a>
+            <a href="#">Otto Mozale</a>
+        </p>
+        
+    </footer>
 </body>
 </html>
+
