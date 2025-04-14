eae10d5a32f8c54ac58bb33d71961b365196a006
Otto Mozale <66283993+OttozaoJooj@users.noreply.github.com>
2025-04-14 09:59:42 -0300

    fix: modal functions bug

--- assets/js/adm.js
+++ assets/js/adm.js
@@ -1,3 +1,58 @@
+let btnDialogUpload = document.querySelector(".btn-upload");
+let dialogUpload = document.querySelector(".dialog-upload-anais");
+let closeDialogUpload = document.querySelector(".close-upload");
+
+let btnDialogDelete = document.querySelectorAll(".btn-delete");
+let dialogDelete = document.querySelector(".dialog-delete-anais");
+let btnDeleteAnais = document.querySelector(".btn-delete-anais");
+let closeDialogDelete = document.querySelector(".btn-delete-anais-close");
+
+
+
+let btnDialogUpdate = document.querySelectorAll(".btn-update");
+let dialogUpdate = document.querySelector(".update-anais");
+let closeDialogUpdate = document.querySelector(".close-update");
+
+
+
+btnDialogUpload.addEventListener("click", function(){
+    dialogUpload.showModal();
+})
+closeDialogUpload.addEventListener("click", function(e){
+    e.preventDefault();
+
+    dialogUpload.close();
+})
+
+btnDialogUpdate.forEach(btn => {
+    btn.addEventListener("click", function(){
+        dialogUpdate.showModal();
+
+    })
+})
+
+closeDialogUpdate.addEventListener("click", function(){
+        dialogUpdate.close();
+})
+
+btnDialogDelete.forEach(btn =>{
+    btn.addEventListener("click", function(){
+        dialogDelete.showModal();
+    })
+})
+
+btnDeleteAnais.addEventListener("click", function(){
+    dialogDelete.close()
+})
+
+closeDialogDelete.addEventListener("click", function(){
+    dialogDelete.close();
+})
+
+
+
+
+
 let anaisElements = document.querySelectorAll(".anais");
 
 btnDelete = document.querySelectorAll(".btn-delete")
@@ -146,57 +201,5 @@ filterElement.addEventListener("change", (e) =>{
 
 
 // Modal Functions
-
-let btnDialogUpload = document.querySelector(".btn-upload");
-let dialogUpload = document.querySelector(".dialog-upload-anais");
-let closeDialogUpload = document.querySelector(".close-upload");
-
-let btnDialogDelete = document.querySelectorAll(".btn-delete");
-let dialogDelete = document.querySelector(".dialog-delete-anais");
-let btnDeleteAnais = document.querySelector(".btn-delete-anais");
-let closeDialogDelete = document.querySelector(".btn-delete-anais-close");
-
-
-
-let btnDialogUpdate = document.querySelectorAll(".btn-update");
-let dialogUpdate = document.querySelector(".update-anais");
-let closeDialogUpdate = document.querySelector(".close-update");
-
-
-
-btnDialogUpload.addEventListener("click", function(){
-    dialogUpload.showModal();
-})
-closeDialogUpload.addEventListener("click", function(e){
-    e.preventDefault();
-
-    dialogUpload.close();
-})
-
-btnDialogUpdate.forEach(btn => {
-    btn.addEventListener("click", function(){
-        dialogUpdate.showModal();
-
-    })
-})
-
-closeDialogUpdate.addEventListener("click", function(){
-        dialogUpdate.close();
-})
-
-btnDialogDelete.forEach(btn =>{
-    btn.addEventListener("click", function(){
-        dialogDelete.showModal();
-    })
-})
-
-btnDeleteAnais.addEventListener("click", function(){
-    dialogDelete.close()
-})
-
-closeDialogDelete.addEventListener("click", function(){
-    dialogDelete.close();
-})
-
 
 
